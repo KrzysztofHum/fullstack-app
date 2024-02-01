726bfdb27241726eb3af6174ee63709d51e9a1c9
KrzysztofHum <k.humienny1996@protonmail.com>
2024-02-01 10:01:43 +0100

    email verification

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,4 +1,4 @@
-import { getVerificationTokenByEmail } from "@/data/verficiation-token";
+import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "./db";
 export const generateVerificationToken = async (email: string) => {
