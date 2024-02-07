ea8dff5287230bcbabbde2ca6c9e5fb0a7b2d794
KrzysztofHum <k.humienny1996@protonmail.com>
2024-02-07 14:36:58 +0100

    example fetch data by client and server component

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,7 +1,9 @@
 import NextAuth, { type DefaultSession } from "next-auth";
+import { boolean } from "zod";
 
 export type ExtendedUser = DefaultSession["user"] & {
   role: "ADMIN" | "USER";
+  isTwoFactorEnabled: Boolean;
 };
 
 declare module "next-auth" {
